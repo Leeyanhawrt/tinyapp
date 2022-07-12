b6df6ba20ea7b7320f9198f682d88f23d767bd21
Leeyanhawrt <leeyan1108@gmail.com>
2022-07-12 13:02:08 +0000

    Add edit button to URL shortener

--- express_server.js
+++ express_server.js
@@ -52,7 +52,12 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls")
-})
+});
+
+app.post("/urls/:id", (req, res) => {
+urlDatabase[req.params.id] = req.body.longURL
+res.redirect("/urls")
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
